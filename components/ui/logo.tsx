f0647935c1a8ad82ff12aa7b480e283b9213a22f
Kilan_vanloo <Kilanvanloo@gmail.com>
2024-12-13 13:38:55 +0100

    fix: type in logo import

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import logo from "@/public/Logo.svg";
+import logo from "@/public/logo.svg";
 
 type LogoProps = {
   className?: string;
